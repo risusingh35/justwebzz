82af5f03380bf0875c6830104f7b6c5a8a0ef8bf
Risu Singh <risusingh35@gmail.com>
2021-11-18 15:02:28 +0530

    18-11-21

---
+++ project-15-11-21/src/index.js
@@ -0,0 +1,14 @@
+
+function tech(evt, tech) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tech).style.display = "block";
+  evt.currentTarget.className += " active";
+}
